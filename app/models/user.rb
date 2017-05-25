bc941ad019096d244b741d0baf309323f0dfae36
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-05-25 16:43:14 +0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ApplicationRecord
 
+  has_many      :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
-  before_save { email.downcase! }
-  validates :name, presence: true, length:{ maximum: 50}
+  before_save    { email.downcase! }
+  validates     :name, presence: true, length:{ maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length:{ maximum: 255},
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  validates     :email, presence: true, length:{ maximum: 255},
+                          format: { with: VALID_EMAIL_REGEX },
+                          uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates     :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -66,6 +67,12 @@ class User < ApplicationRecord
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
